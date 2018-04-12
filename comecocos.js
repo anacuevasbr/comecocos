7e5e2bb621e65f3db9d59a9082b853568aeb2906
Ana Cuevas Bravo <acuevas@beta03.aulas.gsyc.urjc.es>
2018-04-12 10:27:31 +0200

    empezado comecocos

---
+++ comecocos.js
@@ -0,0 +1,75 @@
+//Variables globales
+var Board;
+var canvas;
+var ctx;
+var pixheight;
+var pixwidth;
+
+//Programa principal
+
+function startpacman(){
+  setcanvas();
+  setboard();
+  draw();
+}
+
+function setcanvas(){
+  canvas = document.getElementById("mycanvas");
+  ctx = canvas.getContext("2d");
+  canvas.height = canvas.width;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function setboard(){
+  Board = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+
+  pixwidth = canvas.width/Board.length;
+  pixheight = canvas.height/Board.length;
+}
+
+function draw(){
+  console.log(Board);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var i;
+  var xpos = 0;
+  var ypos = 0;
+
+  for(i = 0;i < Board.length; i++){
+    line = Board[i];
+
+    for(j = 0; j<Board.length;j++){
+      console.log(line[j]);
+      if (line[j] == 0){
+        ctx.fillStyle= "White";
+        ctx.fillRect(xpos, ypos, pixwidth, pixheight);
+      }else{
+        ctx.fillStyle= "Black";
+        ctx.fillRect(xpos, ypos, pixwidth, pixheight);
+      }
+      xpos += pixwidth;
+
+    }
+    xpos = 0;
+    ypos += pixheight;
+    }
+
+}
